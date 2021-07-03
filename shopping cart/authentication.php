21f5428beb64a8cdb8287072c8bfa48f7b80e0fb
Kar-3kn <rpkarthikeyan3301@gmail.com>
2021-07-03 22:10:23 +0530

    Loginpage

---
+++ shopping cart/authentication.php
@@ -0,0 +1,26 @@
+<?php      
+    include('connection.php');  
+    $username = $_POST['user'];  
+    $password = $_POST['pass'];  
+      
+        //to prevent from mysqli injection  
+        $username = stripcslashes($username);  
+        $password = stripcslashes($password);  
+        $username = mysqli_real_escape_string($con, $username);  
+        $password = mysqli_real_escape_string($con, $password);  
+      
+        $sql = "select *from registeration where username = '$username' and password = '$password'";  
+        $result = mysqli_query($con, $sql);  
+        $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
+        $count = mysqli_num_rows($result);  
+          
+        if($count == 1){  
+            setcookie('Username',$username, time()+60*60*7);
+            session_start();
+            $_SESSION['Username']=$username;
+            header("location: firstpage.php");
+        }  
+        else{  
+            echo "<h1> Login failed. Invalid username or password.</h1>";  
+        }     
+?>  
