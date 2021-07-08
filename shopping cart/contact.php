62e00e3438f4e50624b95b6b5343538340cccd2d
Kar-3kn <84977944+Kar-3kn@users.noreply.github.com>
2021-07-08 22:22:57 +0530

    contact_page

---
+++ shopping cart/contact.php
@@ -0,0 +1,103 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport"
+          content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Shopping Cart</title>
+
+    <!-- Font Awesome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/5.8.2/css/all.css" />
+
+    <!-- Bootstrap CDN -->
+    <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+
+    <link rel="stylesheet" href="style.css">
+</head>
+<style>
+.pic
+{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 40%;
+}
+.logo
+{
+  display: block;
+  margin-left: auto;
+  margin-right: auto;
+  width: 3%;
+}
+h2
+{
+  text-align:center;
+}
+
+b
+{
+
+  color:aqua;
+}
+.column {
+  float: left;
+  width: 49.99%;
+  padding: 5px;
+}
+
+/* Clear floats after image containers */
+.row::after {
+  content: "";
+  clear: both;
+  display: table;
+}
+
+</style>
+<body>
+<?php require_once ("php/header.php"); ?>
+<h2> 
+  <p>CONTACT US</p>
+</h2>
+<div class=pic>
+<img src="upload\contact-us-icon-phone-email-260nw-1151872529.jpg" alt="Italian Trulli" >
+
+
+
+<?xml version="1.0" encoding="UTF-8"?>
+
+<description>
+   <b>Are you a customer who needs help? That's what the TEAM is for.</b>
+</description>
+</div>
+<details>
+<name>
+  <p style="color:blue;" ><i> <b>GIFT SHOP </b></i> </p>
+  <p te>A typical online store enables the customer to browse the firm's range of products and services, view photos or images of the products, along with information about the product specifications, features and prices. Online stores usually enable shoppers to use "search" features to find specific models, brands or items.</p>
+</name>
+<contact>
+<p style="color:green;">CONTACT:<i> <b>0413-445433</b></i> </p>
+</contact>
+You're wondering about an order,our products, or our website, shoot them an email at <b> gift@gmail.com</b>.
+</details>
+</br>
+</br>
+
+<socialmedia>
+<div class=logo>
+  <div class="row">
+ <div class="column">
+<a href=""><img src="upload\Instagram-Logo.png"></a>
+</div> 
+<div class="column">
+<a href=""><img src="upload\Facebook-logo.png"></a>
+</div> 
+</div>
+</div>
+</socialmedia>
+
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+</body>
+</html>
