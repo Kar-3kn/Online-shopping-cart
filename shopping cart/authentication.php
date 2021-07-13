79bb25ed89d9b76cee9783c02da379fcd6734394
Kar-3kn <84977944+Kar-3kn@users.noreply.github.com>
2021-07-13 17:03:17 +0530

    logincookies

--- shopping cart/authentication.php
+++ shopping cart/authentication.php
@@ -14,7 +14,13 @@
         $row = mysqli_fetch_array($result, MYSQLI_ASSOC);  
         $count = mysqli_num_rows($result);  
           
-        if($count == 1){  
+        if($count == 1){
+            
+            if(isset($_POST['rememberme']))
+            {
+                setcookie('usercookie',$username, time()+60*60*7);
+                setcookie('passcookie',$password, time()+60*60*7);
+            }
             setcookie('Username',$username, time()+60*60*7);
             session_start();
             $_SESSION['Username']=$username;
